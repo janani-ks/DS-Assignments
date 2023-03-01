93ae5a758952cf57cfcb8d66c54f35bc10d68325
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-03-01 11:34:37 +0530

    Update Hashmap.java

--- DS/src/main/java/collection/collection/Hashmap.java
+++ DS/src/main/java/collection/collection/Hashmap.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 
 public class Hashmap {
 	 static PrintStream display=new PrintStream((new FileOutputStream(FileDescriptor.out)));
-	 public static void main(String args[]){  
+	 public static void main(String[] args){  
 		   HashMap<Integer,String> map=new HashMap<Integer,String>(); 
 		   map.put(3,"Janani");  
 		   map.put(1,"Nivedha");    
